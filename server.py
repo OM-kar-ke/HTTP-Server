108780ee6f261dde5b9259436bb87b7e6b990000
Omkar Karke <omkarkarke25@gmail.com>
2022-05-22 19:28:45 +0530

    Implemented function in run ClientThread class to run the threads and process http requests

--- server.py
+++ server.py
@@ -3,6 +3,11 @@ import sys
 from threading import Thread
 import threading
 
+class HttpMethods:
+    
+    def handle_request():
+        pass
+
 class TCP:
     
 	#Just populating ip_address(host) and port number of server
@@ -55,8 +60,6 @@ class TCP:
 			else:
 				print("\nMaximum Connections reached\n")
 
-class ClientThread(Thread):
-    pass
 
 #Instances of this class are created every time server receives a connection request.
 class ClientThread(Thread):
@@ -68,6 +71,80 @@ class ClientThread(Thread):
 		self.port = port 
 		print ("New server socket thread started for " + ip + ":" + str(port), '\n')
  
+	# This methods executes on a separate thread ,  when an instance of this class calls the start method (Becoz this class is inherited from Thread class).
+	def run(self):
+
+		#Connection: keep-alive/close
+		connection = ['keep-alive']
+
+		# keep_alive = [timeout, max]
+		# max tells the maximum nos of http requests that can be handled through a single tcp connection
+		# it will get overwritten when connection_parameter is overwritten
+		keep_alive = [None, None]
+
+		# this will get over-written in the HttpMethods class
+		connection_parameters = [connection, keep_alive]
+		
+		#Request count for a specific socket(in persistent connection)
+		request_count = 0
+
+		#DOUBT : ithe kay kelay SHETTA kahi samjana
+		self.conn.settimeout(2)
+
+
+		#Accept request on the same connection socket till Connetion(Header in request) is keep-alive.
+		while(connection_parameters[0][0] == 'keep-alive'):
+
+			try:
+			
+				#Recive data(request) from the connection socket.
+				data = self.conn.recv(2048) 
+
+				#If server didn't receive any data
+				if (len(data) == 0):
+					self.conn.close()
+					break;
+
+				#Increment request count, request count is used for maximum nos of request through a single TCP connection
+				request_count += 1
+
+				print("client :" + str(self.ip) +':'+ str(self.port) + '   request count = ', request_count)
+
+				print("\nRequest : \n", data)
+
+				#Created object of HttpMethods class to call handle_request() function
+				http_server_object = HttpMethods()
+				response = http_server_object.handle_request(data, connection_parameters)
+
+
+				# Send response back to client
+				self.conn.send(response)
+
+
+				max_requests = connection_parameters[1][1]
+				if (max_requests != None):
+					max_requests = int(max_requests)
+
+				
+				# If maximum nos of request through the same connections have been made then close the connection.
+				if (request_count == max_requests):
+					self.conn.close()
+					break
+
+				#Setting the timeout value for connection socket, if no request received within this time then throw socket.timeout exception
+				t_out = connection_parameters[1][0]
+				if (t_out == None):
+					t_out = 2		#default server timeout
+				else :
+					t_out = int(t_out)
+     
+				# Setting timeout value here
+				self.conn.settimeout(t_out)
+    
+			except socket.timeout as t:
+				self.conn.close()
+				break
+
 
 if __name__ == '__main__':
     tcp_server = TCP()
