bc9742dd1759551218408b610aa91249088e12d3
Omkar Karke <omkarkarke25@gmail.com>
2022-05-22 19:25:15 +0530

    Implemented function in TCP class to start TCP server

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
+import socket
 import sys
+from threading import Thread
+import threading
 
 class TCP:
     
@@ -10,7 +13,53 @@ class TCP:
 		except Exception as e:
 			self.port = 12000
 
+    #server starts on calling the start_tcp function
+	def start_tcp(self):
+     
+		# create a TCP socket object for listening to TCP requests
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)        		
+		
+		# bind the socket object to the address and port
+		s.bind((self.host, self.port))	        
+		
+		# start listening for connection requests
+		s.listen(int(config['TCP']['queuesize']))
+
+		print("Server is listening at : ", s.getsockname(),'\n')
+
+		# List which stores ClientThreads objects
+		threads = []
+
+		# MAX TCP connections that we can have working simultaneously
+		max_simul_connec = int(config['TCP']['max_conn'])
+
+		# Server is listening
+		# It is waiting on s.accept() till any client makes an http request to it
+		# When some client makes an request, it creates an connection socket for it
+		# Then this http request is processed on new thread
+		while True:
+      
+			# Creates a new connection socket and accept new connection
+			conn, (ip, port) = s.accept()
+			print("\n\nConnected by a client with address : ", ip," ",port)
+
+			if(threading.active_count()<=max_simul_connec):
+				#Creating a new client thread object 
+				newthread = ClientThread(conn, ip, port)
+
+				#Whenever we use multithreading in python we must have a run method in the class(which is inheriting Thread class) to do multi-threading.
+				# The class that inherits Thread class must have a run method in it , which will be executed as a thread function when an instance of that class(child of thread class) calls .start method.
+
+				newthread.start()
+				threads.append(newthread)
+			else:
+				print("\nMaximum Connections reached\n")
+
+class ClientThread(Thread):
+    pass
+
+
 
 if __name__ == '__main__':
     tcp_server = TCP()
-    
+    tcp_server.start_tcp()
